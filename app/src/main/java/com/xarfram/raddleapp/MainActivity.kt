00c96e7e4c1833d1ebd3c506ea54a12396181e98
Morgan Rewis <rewismorgan@gmail.com>
2023-07-09 21:03:43 -0400

    Back Button works properly

--- app/src/main/java/com/xarfram/raddleapp/MainActivity.kt
+++ app/src/main/java/com/xarfram/raddleapp/MainActivity.kt
@@ -1,45 +1,37 @@
 package com.xarfram.raddleapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.webkit.WebView
-import com.xarfram.raddleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private var myWebView: WebView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, true)
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        val myWebView = WebView(applicationContext)
+        myWebView = WebView(applicationContext)
         setContentView(myWebView)
-        myWebView.loadUrl("https://www.raddle.me")
+        myWebView!!.loadUrl("https://www.raddle.me")
     }
 
     override fun onBackPressed() {
-        val myWebView = WebView(applicationContext)
-        if (myWebView.canGoBack()) {
-            myWebView.goBack()
+        if (myWebView?.canGoBack() == true) {
+            myWebView?.goBack()
+        } else {
+            finish()
         }
     }
+
+    /*override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView?.canGoBack() == true) {
+            myWebView?.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no webpage history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event)
+    }*/
 }
